c5f84a8107d19ed6faf2cb6fbd925fa6d7cf3a40
dark-escape <vishalwadhwa13@gmail.com>
2017-05-21 16:34:22 +0530

    minor change

--- gp_php/gupshup/include/config.php
+++ gp_php/gupshup/include/config.php
@@ -5,10 +5,5 @@ define("DB_PASSWORD","");
 define("DB_NAME","gup_shup");
 define("DB_HOST","localhost");
 
-define("GOOGLE_API_KEY","AIzaSyD4T6MC1EDgT0I3IbSenDU1r0hKkPs1ZT0");
-
-// push notification flags
-define('PUSH_FLAG_CHATROOM', 1);
-define('PUSH_FLAG_USER', 2);
-
+define("GOOGLE_API_KEY","your api key");
 ?>
